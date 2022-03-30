951aaa9bc4f5340d225d9366c7d817ef78b846e0
Пользователь <@DESKTOP-C8UBSUL>
2022-03-30 18:57:04 +0300

    Добавьте файлы проекта.

---
+++ Map_Move/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-----------");
+            Console.WriteLine("-         -");
+            Console.WriteLine("-         -");
+            Console.WriteLine("-         -");
+            Console.WriteLine("-         -");
+            Console.WriteLine("-         -");
+            Console.WriteLine("-         -");
+            Console.WriteLine("-         -");
+            Console.WriteLine("-         -");
+            Console.WriteLine("-         -");
+            Console.WriteLine("-----------");
+            Console.SetCursorPosition(5, 5);
+            ConsoleKeyInfo input = Console.ReadKey();
+            switch (input.Key)
+            {
+                case ConsoleKey.W:
+                    {
+                        Console.SetCursorPosition(6, 5);
+                        break;
+                    }
+                case ConsoleKey.A:
+                    {
+                        Console.SetCursorPosition(4, 5);
+                        break;
+                    }
+                case ConsoleKey.S:
+                    {
+                        Console.SetCursorPosition(5, 6);
+                        break;
+                    }
+                case ConsoleKey.D:
+                    {
+                        Console.SetCursorPosition(6, 5);
+                        break;
+                    }
+            }
+            Console.ReadLine();
+        }
+    }
+}
